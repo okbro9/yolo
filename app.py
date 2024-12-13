5e90c3972c965be5c074e34a372acb0a271cf53d
okbro9 <166011239+okbro9@users.noreply.github.com>
2024-12-13 15:42:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,6 @@ import time
 # Title and description for the Streamlit app
 st.title("Real-time Object Detection with YOLOv8")
 st.text("Using a webcam feed")
+
+# Streamlit widget to select source: webcam or video
+source = st.sidebar.selectbox("Select input source", ["Webcam", "Video"])
